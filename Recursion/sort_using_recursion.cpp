a01d2965327a2929a2a8df98dec9c76ef249763c
vaibhav_pat <vaibhavpatidar987@gmail.com>
2025-04-19 10:53:56 +0530

    added sort_using_recursion

---
+++ Recursion/sort_using_recursion.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void insert(vector<int> &v, int val){
+    if(v.size() == 0) return;//BC
+    else if(v[v.size()-1]<val){//BC
+        v.push_back(val);return;
+    }
+    int rem = v.back();//H
+    v.pop_back();//H
+    insert(v,val);//H
+    v.push_back(rem);// Induction
+    return;
+}
+void sort(vector<int> &v){
+    if(v.size() == 1) return; // BC
+    int back = v[v.size()-1]; // Hyothesis
+    v.pop_back();//Hypothesis
+    sort(v);//Hypothesis
+    insert(v,back);//Induction
+    return;
+}
+void print(vector<int> v ){
+    if(v.size() == 0)return;//BC
+    int temp = v.back();//H
+    v.pop_back();//H
+    print(v);//H
+    cout<<temp<<" ";//Induction
+    return;
+}
+int main(){
+    int n ;
+    cin >> n;
+    vector<int> v(n);
+    for(int i = 0; i < n ; i++){
+        cin>>v[i];
+    }
+    sort(v);//Recursive Function
+    print(v);//Recursive Function
+}
